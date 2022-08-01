ae79b0a1fc29736351a3553d8f19f9f373e81656
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-01 14:49:37 -0700

    add images for slider

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,5 +1,17 @@
 export const SliderData = [
   {
     image: 'https://images.unsplash.com/photo-1658995901444-e5adc8c3f420?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ2fGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1532135468830-e51699205b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQyfGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1658651266023-058fb0e7f7a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1654010728791-b7d5cecbeed5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1659286469999-fa1862735ce4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
   }
-]
+];
