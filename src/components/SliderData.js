f668194a766d4ff196aaae14e725efba5465618d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-01 14:45:42 -0700

    initial set up commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,5 @@
+export const SliderData = [
+  {
+    image: 'https://images.unsplash.com/photo-1658995901444-e5adc8c3f420?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ2fGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  }
+]
